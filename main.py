ebf3a05c176bb5e640be3a905274beda8a0aa4f9
nek00oo <yakovenkovalera2017@yandex.ru>
2025-07-28 14:34:07 +0300

    fix3

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ def greet(name):
     if not name:
         return "Hello, stranger! No name provided."
     print("Hiiii")
-    return f"HELLO, {name.upper()}! WELCOME ABOARD!"
+    return f"HELLO, {name.upper()}! WELCOME USER!"
 
 def calculate_area(length, width):
     return length * width
 
 if __name__ == "__main__":
-    print(greet("Alice"))
+    print(greet("Mark"))
     print("Area of rectangle:", calculate_area(5, 3))
